c6000140f611b02cb6623d24300ed5ec9c3214a3
leandro <leandrohmirandacontato@gmail.com>
2023-06-23 17:46:38 -0300

    Primeiro commit

---
+++ pdf.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div id="div_campos" class="col-md-12">oi eu sou o goku</div>
+    <button type="button" id="btn-print" class="btn btn-default"><i class="fas fa-download"></i> Baixar PDF</button>
+
+    <script>
+        document.getElementById('btn-print').addEventListener('click', function() {
+            var divConteudo = document.getElementById('div_campos').innerHTML;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'gerar_pdf.php');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.responseType = 'blob';
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    var blob = new Blob([xhr.response], {
+                        type: 'application/pdf'
+                    });
+                    var link = document.createElement('a');
+                    link.href = window.URL.createObjectURL(blob);
+                    link.download = 'nome_arquivo.pdf';
+                    link.click();
+                }
+            };
+            var formData = new FormData();
+            formData.append('conteudo', divConteudo);
+            xhr.send(formData);
+        });
+    </script>
+</body>
+
+</html>
+<?php
+if (isset($_POST['conteudo'])) {
+    require('FPDF/fpdf.php');
+
+    $conteudo = $_POST['conteudo'];
+
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(40, 10, $conteudo);
+
+    $pdf->Output('nome_arquivo.pdf', 'D');
+    exit;
+}
+?>
