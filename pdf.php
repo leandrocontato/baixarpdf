273fd8d0b26cbdde661e0877ee7e65617d163b0a
leandro <leandrohmirandacontato@gmail.com>
2023-06-23 18:16:17 -0300

    pdf

--- pdf.php
+++ pdf.php
@@ -36,18 +36,3 @@
 </body>
 
 </html>
-<?php
-if (isset($_POST['conteudo'])) {
-    require('FPDF/fpdf.php');
-
-    $conteudo = $_POST['conteudo'];
-
-    $pdf = new FPDF();
-    $pdf->AddPage();
-    $pdf->SetFont('Arial', 'B', 16);
-    $pdf->Cell(40, 10, $conteudo);
-
-    $pdf->Output('nome_arquivo.pdf', 'D');
-    exit;
-}
-?>
